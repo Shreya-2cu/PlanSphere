59c3102d895a7a7b8503f72cd55e28bd9dff7576
Shreya-2cu <dwivedishreya66115@gmail.com>
2025-05-27 09:30:13 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
-/// <reference types="node" />
-import type * as scandir from '@nodelib/fs.scandir';
-export declare type Entry = scandir.Entry;
-export declare type Errno = NodeJS.ErrnoException;
-export interface QueueItem {
-    directory: string;
-    base?: string;
-}
+// Declaration files in this directory contain types relating to TypeScript library features
+// that are not included in all TypeScript versions supported by DefinitelyTyped, but
+// which can be made backwards-compatible without needing `typesVersions`.
+// If adding declarations to this directory, please specify which versions of TypeScript require them,
+// so that they can be removed when no longer needed.
+
+/// <reference path="disposable.d.ts" />
+/// <reference path="indexable.d.ts" />
+/// <reference path="iterators.d.ts" />
